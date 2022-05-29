36adfd8bad98b4a0cbe1269eaf6aa1b7e22f2e2d
Sakar <techiesakar@gmail.com>
2022-05-29 13:54:45 +0545

    Search Pagination - Product Count Added - productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,6 +15,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 // Get all Products
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
   const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
     .filter()
@@ -23,6 +24,7 @@ exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     products,
+    productCount,
   });
 });
 
